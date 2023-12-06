4baab8fb8147975d1219f3a2ab133a5dc84e9b3a
Rohan Viswanathan <rohanvis@berkeley.edu>
2023-12-06 23:27:50 +0000

    Call nn module super

--- mineclip/mineagent/features/prompt_enc/allMiniLM_prompt_feat.py
+++ mineclip/mineagent/features/prompt_enc/allMiniLM_prompt_feat.py
@@ -6,7 +6,11 @@ import torch.nn as nn
 
 class PromptEmbFeat(nn.Module):
     def __init__(self, device):
+        super().__init__()
         self.model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
 
     def embed(self, x):
         return self.model.encode(x) # Returns 384 dimensional embedding
+
+    def forward(self, x, **kwargs):
+        return x, None
